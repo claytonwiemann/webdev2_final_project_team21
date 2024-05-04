5abedb2ba53b78a4a7393d3c57872c69323b033e
claytonwiemann <78102051+claytonwiemann@users.noreply.github.com>
2024-05-04 13:19:28 -0500

    service.js added and added methods in there. Also tested it with the currency component

--- angular-final-project/src/app/currency/currency.component.ts
+++ angular-final-project/src/app/currency/currency.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from './currency.service'; // Import the CurrencyService
 
 @Component({
   selector: 'app-currency',
   templateUrl: './currency.component.html',
-  styleUrl: './currency.component.css'
+  styleUrls: ['./currency.component.css']
 })
-export class CurrencyComponent {
+export class CurrencyComponent implements OnInit {
+  exchangeRates: any; // Define a property to hold the exchange rates
 
+  constructor(private currencyService: CurrencyService) { } // Inject the CurrencyService
+
+  ngOnInit(): void {
+    // Call the fetchExchangeRates method from the CurrencyService
+    this.currencyService.fetchExchangeRates('USD').subscribe(
+      (data: any) => {
+        this.exchangeRates = data;
+      },
+      (error: any) => {
+        console.error('Error fetching exchange rates:', error);
+      }
+    );
+  }
 }
