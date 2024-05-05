09dc4050a4446675826c374de0c78decb39b0c21
bdbhb6 <bryson.bourisaw@gmail.com>
2024-05-04 22:49:20 -0500

    styling and home page

--- angular-final-project/src/app/app-routing.module.ts
+++ angular-final-project/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { WeatherComponent } from './weather/weather.component';
 import { CurrencyComponent } from './currency/currency.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent},
   { path: 'weather', component: WeatherComponent }, // Weather page route
   { path: 'currency', component: CurrencyComponent }, // Currency page route
   { path: '**', redirectTo: '' } // Redirect to Home page for unknown routes
