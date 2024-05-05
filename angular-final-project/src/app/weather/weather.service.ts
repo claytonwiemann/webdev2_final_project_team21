746fef73163b7292148746abcc94ad3db22f739f
jamesgrady <zgrady@gmail.com>
2024-05-04 22:08:29 -0500

    Update weather.service.ts

--- angular-final-project/src/app/weather/weather.service.ts
+++ angular-final-project/src/app/weather/weather.service.ts
@@ -8,7 +8,35 @@ import { Observable } from 'rxjs';
 export class WeatherService {
   constructor(private http: HttpClient) {}
 
+  private latitude = 0;
+  private longitude = 0;
+
   fetchWeatherByCoordinates(latitude: number, longitude: number): Observable<any> {
-    return this.http.get<any>(`http://localhost:3000/weather?lat=${latitude}&lon=${longitude}`);
+    return this.http.get<any>(`http://localhost:3000/weather?lat=${this.latitude}&lon=${this.longitude}`);
+  }
+
+  setCoordinates(latitude: number, longitude: number) {
+    this.latitude = latitude;
+    this.longitude = longitude
+  }
+
+  getLatitude() {
+    return this.latitude;
+  }
+
+  getLongitude() {
+    return this.longitude;
+  }
+
+  getWeatherData() {
+    return this.http.get<any>(`http://localhost:3000/weather?lat=${this.latitude}&lon=${this.longitude}`);
+  }
+
+  ngOnInit() {
+    return this.http.get<any>(`http://localhost:3000/weather?lat=${this.latitude}&lon=${this.longitude}`);
+  }
+
+  ngOnDestroy() {
+
   }
 }
